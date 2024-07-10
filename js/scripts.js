355fce791dae828687c92030e29122941c823676
Claudio-Pri <priv.claudio@gmail.com>
2024-07-10 16:53:28 +0200

    Aggiunto file js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+
+document.writeln('Hello world');
+
+
+
+// fa uscire un pop-up che blocca l'esecuzione della pagina
+alert('Hello world!');
+
+
+
+//prende un elemento dal documento e lo trasforma--js è case sensitive
+document.getElementById('mio_id').innerHTML = 'Hello World 4!';
+
+
+
+//come conservare un dato
+/* 
+variabili: contenitori con un nome e una proprietà/ dato
+i dati della variabile vengono eliminati alla fine dell'esecuzione 
+*/
+
+//Dichiarazione di una variabile
+
+const miaVariabile1 = 'ciao raghi';
+let miaVariabile2;
+var miaVariabile3;
+
+
