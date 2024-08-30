d557e03bfccd635b9c82f2a44c8e311efe4c2c52
Claudio-Pri <priv.claudio@gmail.com>
2024-08-30 15:33:09 +0200

    template Vue js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,22 @@ console.log('js collegato');
 //===== Appunti ======
 // ` backtick
 
+// {{TEXT INTERPOLATION}}
+
 //===== Code ======
 
 
 //===== Functions =====
+
+// deconstructuring
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Ciao Vue!'
+        }
+    }
+
+
+}).mount('#app')
